0f666803a19be145fd0c6ecf80c5ae8b588bf759
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-09-15 06:57:07 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 apt update
-apt install python3-pip
+apt install python3-pip -y
 pip install jupyter 
 echo -e "ekral\nekral" | jupyter notebook password 
 jupyter notebook --ip 0.0.0.0 --port 8080 --allow-root  --no-browser
