0364b80db00af2484aa8acc548c0c9883c8832d8
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-09-15 20:18:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
-from http.server import SimpleHTTPRequestHandler
-from socketserver import TCPServer
-import subprocess
-PORT = 8080
+from flask import Flask
 
-class MyHandler(SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(b'Merhaba, dunya!')
-        try:
-          subprocess.run("python3 main.py", shell=True).decode('utf-8')
-        except:
-          print("Zaten aktif")
-httpd = TCPServer(("", PORT), MyHandler)
-print("Web sunucusu çalışıyor...")
-httpd.serve_forever()
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Web App with Python Flask!'
+
+app.run(host='0.0.0.0', port=80)
