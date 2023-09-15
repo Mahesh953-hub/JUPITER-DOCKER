646a348ed7d2f9b2b191d03b0d9509a80af58c45
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-09-15 21:01:04 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 apt update
-apt install python3-pip -y
+apt install npm -y
 apt install git -y
 
-git clone https://github.com/ErdemBey31/kelimebot
-cd kelimebot
-pip install -r requirements.txt
-pip install gunicorn flask
-gunicorn app:app & python3 -m kelime_bot
+git clone https://github.com/ErdemBey31/teslagamev1
+cd teslagamev1 
+npm install
+node botTesla.js &
