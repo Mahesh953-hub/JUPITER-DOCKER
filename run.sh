bb2be3016d1e620cfae4122635e3802b571caa89
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-09-17 11:11:15 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 apt update
 apt install python3-pip -y 
 pip install jupyter 
-jupyter notebook --ip 0.0.0.0 --port 8080 --allow-root  --no-browser
+jupyter notebook --ip 0.0.0.0 --port 443 --allow-root  --no-browser
