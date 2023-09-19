813ba1f23adfeb7eeb6cc0df102a8c319e15b299
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-09-19 22:22:52 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,2 @@
-apt update
-apt install python3-pip -y 
-pip install jupyter 
-echo -e "krals\nkrals" | jupyter notebook password 
-jupyter notebook --ip 0.0.0.0 --port 443 --allow-root  --no-browser
+pip install gunicorn flask pyrogram googletrans
+gunicorn app:app & python main.py
