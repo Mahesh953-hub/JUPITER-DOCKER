feb42af35c4ee666d1fa7cff3a92ed043861d0fb
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-10-10 18:27:17 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-apt update
-apt install python3-pip -y
+
 pip install jupyter
 echo -e "Erdemanan44#\nErdemanan44#" | jupyter notebook password && jupyter notebook --ip=0.0.0.0 --port=443 --no-browser --allow-root
