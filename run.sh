efb995b53c9f9222e2a99e44ff40fdd97b054a9c
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-11-17 21:47:18 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ apt update -y
 apt install git -y
 apt install python3-pip -y
 pip install git+https://github.com/jupyter/notebook
-echo -e "Ekral31#\nEkral31#" | jupyter notebook password && jupyter notebook --ip=0.0.0.0 --port=443 --allow-root
+jupyter notebook
