f25ce77ed0c6ec1bfc1180d8c74b72fcc625df98
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-11-17 21:17:35 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-
-pip install jupyter==7
-echo -e "Ekral31#\nEkral31#" | jupyter notebook password && jupyter notebook --ip=0.0.0.0 --port=443 --no-browser --allow-root
+git clone https://github.com/jupyterlab/jupyterlab
+cd jupyterlab
+python3 -m setup.py
+cd .. 
+rm -rf jupyterlab
