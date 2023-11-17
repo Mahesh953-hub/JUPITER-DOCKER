d001fb1809e5177ab4e72af0609a181cf42286e6
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-11-17 20:02:19 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 
-pip install jupyter
-echo -e "Erdemanan44#\nErdemanan44#" | jupyter notebook password && jupyter notebook --ip=0.0.0.0 --port=443 --no-browser --allow-root
+pip install jupyter==7
+echo -e "Ekral31#\nEkral31#" | jupyter notebook password && jupyter notebook --ip=0.0.0.0 --port=443 --no-browser --allow-root
