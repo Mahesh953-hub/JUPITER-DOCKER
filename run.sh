dbe2ee788c50034b4e80df66033effc763456923
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-11-17 22:06:35 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 apt update -y
 apt install git -y
 apt install python3-pip -y
-pip install git+https://github.com/jupyter/notebook
-jupyter notebook
+git clone https://github.com/jupyter/notebook.git
+pip install -r notebook/requirements.txt
+python notebook/notebookapp.py
