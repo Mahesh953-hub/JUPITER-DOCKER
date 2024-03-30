e70e534f76a611613b1c2c2987b361db8513f87f
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2024-03-30 10:23:50 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 pip install notebook
-echo -e "Ekral31#\nEkral31#" | jupyter notebook password && jupyter notebook --ip=0.0.0.0 --port=443 --allow-root
+echo -e "e\ne" | jupyter notebook password && jupyter notebook --ip=0.0.0.0 --port=443 --allow-root
